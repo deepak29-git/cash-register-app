e7e9ea23bb0baa5f56e398c232e7bfa8f1884e4d
Deepak Goyal <dg887180@gmail.com>
2022-01-12 17:34:16 +0530

    convert to es6

--- app.js
+++ app.js
@@ -10,70 +10,92 @@ const nextBtn = document.querySelector("#next-button");
 const cashGivenHeading = document.querySelector("#heading-2");
 
 const totalNote = [2000, 500, 100, 20, 10, 5, 1];
-nextBtn.addEventListener("click", dataNext)
-hidePart();
-function dataNext() {
+
+const showcashGivenSection=()=>{
     cashGiven.style.display = "block";
     cashGivenHeading.style.display = "block";
     checkBtn.style.display = "block";
-    if (Number(billAmount.value) > 0) {
-        nextErrorMsg.style.display = "none";
-    } else {
+    }
+    
+    const checkHideValidation=()=>{
+        if (Number(billAmount.value) > 0) {
+            nextErrorMsg.style.display = "none";
+        } else {
+            cashGiven.style.display = "none";
+            cashGivenHeading.style.display = "none";
+            checkBtn.style.display = "none";
+            nextErrorMsg.innerText = "Enter the valid amount";
+        }
+    }
+
+    const hidePart=()=> {
         cashGiven.style.display = "none";
         cashGivenHeading.style.display = "none";
         checkBtn.style.display = "none";
-        nextErrorMsg.innerText = "Enter the valid amount";
-    }
-
-}
-function hidePart() {
-    cashGiven.style.display = "none";
-    cashGivenHeading.style.display = "none";
-    checkBtn.style.display = "none";
-    returnChange.style.display = "none";
-    tableDisplay.style.display = "none";
-}
-
-checkBtn.addEventListener("click", dataCheck)
-function dataCheck() {
-    hideMessage();
-    returnChange.style.display = "block";
-    tableDisplay.style.display = "block";
-    if (Number(billAmount.value) > 0) {
-        if (Number(cashGiven.value) >= Number(billAmount.value)) {
-            const returnAmount = (Number(cashGiven.value) - Number(billAmount.value));
-            calculateNote(returnAmount);
-
-        } else {
-            returnChange.style.display = "none";
-            tableDisplay.style.display = "none";
-            displayMessage("cash given amount should be greater than bill amount")
-        }
-
-
-    } else {
         returnChange.style.display = "none";
         tableDisplay.style.display = "none";
-        displayMessage("amount should be greater than 0")
     }
 
+const dataNext=()=> {
+ 
+    showcashGivenSection()
+    checkHideValidation()
+}
 
 
-    function calculateNote(returnAmount) {
+
+const dataCheck=()=>{
+     const hideMessage=()=> {
+        errorMsg.style.display = "none";
+    }
+    hideMessage();
+
+    const displayResult=()=>{
+        returnChange.style.display = "block";
+        tableDisplay.style.display = "block";
+    }
+    
+    displayResult();
+
+    
+    const displayMessage=(msg)=> {
+        errorMsg.style.display = "block";
+        errorMsg.innerText = msg;
+    }
+    
+    
+    const calculateNote=(returnAmount)=> {
         for (let i = 0; i < totalNote.length; i++) {
             const numberOfNotes = Math.floor(returnAmount / totalNote[i]);
             returnAmount = returnAmount % totalNote[i]
             noOfNote[i].innerText = numberOfNotes;
-
+            
         }
     }
-
-    function hideMessage() {
-        errorMsg.style.display = "none";
+    
+    const checkValidation=()=>{
+        if (Number(billAmount.value) > 0) {
+            if (Number(cashGiven.value) >= Number(billAmount.value)) {
+                const returnAmount = (Number(cashGiven.value) - Number(billAmount.value));
+                calculateNote(returnAmount);
+                
+            } else {
+                returnChange.style.display = "none";
+                tableDisplay.style.display = "none";
+                displayMessage("cash given amount should be greater than bill amount")
+            }
+            
+            
+        } else {
+            returnChange.style.display = "none";
+            tableDisplay.style.display = "none";
+            displayMessage("amount should be greater than 0")
+        }
+    
     }
-
-    function displayMessage(msg) {
-        errorMsg.style.display = "block";
-        errorMsg.innerText = msg;
-    }
+    checkValidation();
+        
 }
+nextBtn.addEventListener("click", dataNext)
+checkBtn.addEventListener("click", dataCheck)
+hidePart();
